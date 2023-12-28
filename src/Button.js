ee787db831933b1504c08b305f72d545d55b26a6
Piranavan <piranavanpiran2000@gmail.com>
2023-12-28 20:36:02 +0530

    add

---
+++ src/Button.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+function Button() {
+  return (
+    <div className="Button">
+      
+      <button>submit</button>
+      <Text></Text>
+
+    </div>
+  );
+}
+
+function Text() {
+    return (
+      <div className="Button">
+        <p>Iam Piranavan</p>
+      </div>
+    );
+  }
+
+
+export default Button;
